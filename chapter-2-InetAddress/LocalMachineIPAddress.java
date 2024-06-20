9e3ac4dfda30afe4e1a4b27417070a6b4f885826
Sagar Raut <sagarraut2059@gmail.com>
2024-06-20 17:28:28 +0545

    new code

---
+++ chapter-2-InetAddress/LocalMachineIPAddress.java
@@ -0,0 +1,14 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class LocalMachineIPAddress {
+    public static void main(String[] args) {
+        try {
+            InetAddress localHost = InetAddress.getLocalHost();
+            System.out.println("Local IP Address: " + localHost.getHostAddress());
+            System.out.println("Local Hostname: " + localHost.getHostName());
+        } catch (UnknownHostException e) {
+            System.err.println("Error occurred: " + e.getMessage());
+        }
+    }
+}
