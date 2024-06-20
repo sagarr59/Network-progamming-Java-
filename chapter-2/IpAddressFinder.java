29f9c291f00c98013f57a1fda138e04beca170fc
Sagar Raut <sagarraut2059@gmail.com>
2024-06-20 17:06:47 +0545

    file name changed

---
+++ chapter-2/IpAddressFinder.java
@@ -0,0 +1,13 @@
+import java.net.*;
+public class IpAddressFinder{
+    public static void main(String[] args) {
+        try{
+            InetAddress address= InetAddress.getByName("www.tufohss.edu.np");
+            System.out.println(address.getHostName());
+            System.out.println(address.getHostAddress());
+        }
+        catch(Exception e){
+            System.out.println("Error Message:"+e);
+        }
+    }
+}
