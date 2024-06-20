bddd6084749431a104acd87b61fd0665adc62370
Sagar Raut <sagarraut2059@gmail.com>
2024-06-20 19:47:22 +0545

    new code

---
+++ chapter-2-InetAddress/CanonicalHostname.java
@@ -0,0 +1,12 @@
+import java.net.*;
+public class CanonicalHostname {
+    public static void main(String[] args) {
+     try {
+            InetAddress address = InetAddress.getByName("93.184.216.34");
+            String canonicalHostname = address.getCanonicalHostName();
+            System.out.println("Canonical Hostname: " + canonicalHostname);
+        } catch (UnknownHostException e) {
+            System.out.println("Unknown host: "+e );
+        }
+    }
+}
