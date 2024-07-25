ed659b5f928f5f8f266ec2c9cd6b03c867a38974
Sagar Raut <sagarraut2059@gmail.com>
2024-07-25 20:36:09 +0545

    new code

---
+++ chapter-3-URLs&URIs/ProtocolTest.java
@@ -0,0 +1,31 @@
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class ProtocolTest {
+    public static void main(String[] args) {
+        // List of protocols to test
+        String[] urls = {
+            "http://google.com",
+            "https://google.com",
+            "ftp://google.com",
+            "file:///C:/Users/Public/Documents/",
+            "mailto:example@example.com"
+        };
+
+        for (String url : urls) {
+            testProtocol(url);
+        }
+    }
+
+    static void testProtocol(String url) {
+        try {
+            // Attempt to create a URL object
+            URL u = new URL(url);
+            System.out.println(u.getProtocol() + " is supported");
+        } catch (MalformedURLException ex) {
+            // If an exception is thrown, the protocol is not supported
+            String protocol = url.substring(0, url.indexOf(":"));
+            System.out.println(protocol + " is not supported");
+        }
+    }
+}
---
+++ chapter-3-URLs&URIs/URLEncodedDemo.java
@@ -0,0 +1,25 @@
+// Java program to demonstrate the x-www-form-urlencoded strings
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+public class URLEncodedDemo {
+    public static void main(String[] args) {
+        // Example data
+        String originalString = "name=Sagar Raut&age=22&city=Kathmandu";
+        
+        try {
+            // Encode the string
+            String encodedString = URLEncoder.encode(originalString, StandardCharsets.UTF_8.toString());
+            System.out.println("Encoded String: " + encodedString);
+            
+            // Decode the string
+            String decodedString = URLDecoder.decode(encodedString, StandardCharsets.UTF_8.toString());
+            System.out.println("Decoded String: " + decodedString);
+            
+        } catch (UnsupportedEncodingException e) {
+            System.err.println("Encoding/Decoding error: " + e.getMessage());
+        }
+    }
+}
