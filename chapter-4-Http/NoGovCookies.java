a5a488562bc10457d8d9749b2e6f1c1a78181c3f
Sagar Raut <sagarraut2059@gmail.com>
2024-08-10 19:32:28 +0545

    lab4

--- chapter-4-Http/NoGovCookies.java
+++ chapter-4-Http/NoGovCookies.java
@@ -5,10 +5,16 @@ import java.net.URI;
 public class NoGovCookies implements CookiePolicy {
     @Override
     public boolean shouldAccept(URI uri, HttpCookie cookie) {
-        if (uri.getAuthority().toLowerCase().endsWith(".gov")
-                || cookie.getDomain().toLowerCase().endsWith(".gov")) {
-            return false;
-        }
-        return true;
+        return !(uri.getAuthority().toLowerCase().endsWith(".gov")
+                || cookie.getDomain().toLowerCase().endsWith(".gov"));
+    }
+
+    public static void main(String[] args) {
+        URI uri = URI.create("http://www.usa.gov");
+        HttpCookie cookie = new HttpCookie("session", "12345");
+        cookie.setDomain("usa.gov");
+
+        System.out.println("Cookie Domain: " + cookie.getDomain());
+        System.out.println(new NoGovCookies().shouldAccept(uri, cookie) ? "Cookie accepted" : "Cookie blocked");
     }
 }
