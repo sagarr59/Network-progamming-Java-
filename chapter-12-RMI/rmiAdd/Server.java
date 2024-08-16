b381ca5ebf76f577daa748f4cd6b541660cf8433
Sagar Raut <sagarraut2059@gmail.com>
2024-08-16 21:47:25 +0545

    add

--- chapter-12-RMI/rmiAdd/Server.java
+++ chapter-12-RMI/rmiAdd/Server.java
@@ -4,6 +4,6 @@ import java.rmi.*;
 public class Server {
     public static void main(String[] args) throws RemoteException, MalformedURLException {
         AddImp a2 = new AddImp();
-        Naming.rebind("rmi://localhost/Add", a2);
+        Naming.rebind("rmi://localhost/Add", a2);//rebind 
     }
 }
